73af168b22b03e8ab53001e8776ec9663bbedde6
CountryRedneck032311 <thegunnerpaxton@gmail.com>
2026-09-22 13:37:13 -0500

    Update

---
+++ playlist.py
@@ -0,0 +1,18 @@
+# playlist.py
+
+SONGS = [
+    ("Imagine", "John Lennon"),
+    ("Blinding Lights", "The Weeknd"),
+    ("Viva La Vida", "Coldplay"),
+     ("Three Wooden Crosses", "Alan Jackson"),
+    ]
+
+
+def print_playlist(songs):
+    print("My Playlist")
+    for number, (title, artist) in enumerate(songs, start=1):
+        print(f"{number}. {title} - {artist}")
+    print(f"Total songs: {len(songs)}")
+
+if __name__ == "__main__":
+    print_playlist(SONGS)
